7bb0abafe1bae0362d5c4b600134c16e70aeba81
Bruno Pinto Castilho <1600492@SALAS.AULAS>
2019-05-16 20:03:59 -0300

    alterações AC7 e AC8 - ORM

--- AC/core/models.py
+++ AC/core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+class cadastro(models.Model):
+    usuario = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    senha = models.CharField(max_length=50, blank=False)
+    confirmaSenha = models.CharField(max_length=50, blank=False)
+    endereco = models.CharField(max_length=50, blank=True)
+    cidade = models.CharField(max_length=50, blank=True)
+    estado = models.CharField(max_length=50, blank=True)
+    cep = models.CharField(max_length=50, blank=True)
+
+
+    def __str__(self):
+        return self.usuario
+
+class autenticacao(models.Model):
+    usuario = models.CharField(max_length=50, blank=False)
+    senha = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.usuario
+        
 # Create your models here.
